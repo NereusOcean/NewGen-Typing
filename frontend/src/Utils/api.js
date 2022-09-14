55aa677ba386a3e7d200a9626c59c25cef175e45
NereusOcean <niki_5555@mail.ru>
2022-09-14 20:20:10 +0300

    Network Eror
    Get message if get Network message

--- frontend/src/Utils/api.js
+++ frontend/src/Utils/api.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 async function getText(language) {
     let data;
     if (language === 'en') {
-        data = await axios.get(`https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1`)
-            .then((answ) => answ.data)
-            .catch((err) => console.log(err.toJSON()));
+            data = await axios.get(`https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1`)
+                .then((answ) => answ.data[0])
+                .catch((err) => err.toJSON().message);
+
     } else if (language === 'ru') {
-        data = await axios.get(`https://fish-text.ru/get?format=string&number=1`)
-            .then((answ) => answ.data.text)
-            .catch((err) => console.log(err.toJSON()));
+            data = await axios.get(`https://fish-text.ru/get?format=string&number=1`)
+                .then((answ) => answ.data.text)
+                .catch((err) => err.toJSON().message);
     }
 
     return data;
